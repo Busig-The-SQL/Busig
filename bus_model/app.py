1d82650c0b54dcae6f885eb1dda97f325b9eb926
Liam Cotter <122333311@umail.ucc.ie>
2025-04-04 00:38:35 +0100

    Move setup code into app factory, improve configs, fix Dockerfile CMD for calling app factory

--- bus_model/app.py
+++ bus_model/app.py
@@ -1,46 +1,41 @@
-from helpers import transit_entities as model
 from config import ProductionConfig
-
-import datetime
-import os
-import requests
-import subprocess
-import time
-
-from flask import Flask, g, abort, jsonify, request, has_request_context
-from helpers.gtfsr import GTFSR, StaticGTFSR, BustimesAPI
-from GTFS_Static.db_funcs import get_route_id_to_name_dict
-from dotenv import load_dotenv
-from math import ceil
+from flask import Flask
 from routes.all_routes import app as bp
-from routes.all_routes import update_bus, update_realtime
 
-
-subprocess.Popen(["service", "cron", "start"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-
-load_dotenv()
-training_uri = os.getenv("TRAINING_URI")
 
 def create_app(config=ProductionConfig):
     """A Flask App Factory."""
     app = Flask(__name__)
-    if config:
-        app.config.from_object(config)
+    app.config.from_object(config)
+    app.register_blueprint(bp)
+
+    if app.config["APP_FACTORY_ONLY"]:
+        return app
+    
+    # Imports
+    from helpers.gtfsr import StaticGTFSR
+    from dotenv import load_dotenv
+    from routes.all_routes import update_bus, update_realtime
+
+    import subprocess
+    import time
+
+    # Pre Flask app setup
+    subprocess.Popen(["service", "cron", "start"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+    load_dotenv()
+    
+    # Post Flask app setup
+
+    app.config["healthy"] = False
+    load_before = time.time()
+    StaticGTFSR.load_all_files()
+    update_bus()                    # Update the bus data on startup
+    update_realtime()               # Update the realtime data on startup
+    print(f"Loaded in {time.time() - load_before} seconds")
+    print("Loaded")
+
+    app.config["healthy"] = True
     return app
 
 app = create_app()
-app.register_blueprint(bp)
-
-app.config["healthy"] = False
-
-load_before = time.time()
-StaticGTFSR.load_all_files()
-
-
-
-update_bus()                    # Update the bus data on startup
-update_realtime()               # Update the realtime data on startup
-print(f"Loaded in {time.time() - load_before} seconds")
-print("Loaded")
-
-app.config["healthy"] = True
--- bus_model/config.py
+++ bus_model/config.py
@@ -2,15 +2,15 @@ class BaseConfig:
     TESTING = False
     DEBUG = False
     MAKE_DB = True
+    APP_FACTORY_ONLY = False
 
 class TestConfig(BaseConfig):
     """Test configuration."""
     TESTING = True
     DEBUG = True
     MAKE_DB = False
+    APP_FACTORY_ONLY = True
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
-    TESTING = False
-    DEBUG = False
-    MAKE_DB = True
+    ...
