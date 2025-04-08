0d10b99c710f77ce3d586d8f1a5392ce10c53adc
Liam Cotter <122333311@umail.ucc.ie>
2025-04-08 23:55:25 +0100

    Add /v1/stops/ test for bus-model

--- bus_model/tests/test_bus-model_api.py
+++ bus_model/tests/test_bus-model_api.py
@@ -5,3 +5,27 @@ def test_home(client: FlaskClient) -> None:
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == {'message': 'Hello, World!'}
+
+def test_v1_get_all_stops(client: FlaskClient, all_test_objects) -> None:
+    """Tests the /v1/stops endpoint."""
+    response = client.get('/v1/stops')
+    assert response.status_code == 200
+    json = response.json
+    assert type(json) == list, f"Response is not a list, {type(json)} instead"
+    assert len(json) == 2, f"Response of length 2 != {len(json)}"
+
+    keys = ["id", "name", "code", "lat", "lon"]
+    for key in keys:
+        assert key in json[0], f"Key {key} not in first stop data"
+        assert key in json[1], f"Key {key} not in second stop data"
+    
+    key_type_pairs = [
+        ("id", str),
+        ("name", str),
+        ("code", str),
+        ("lat", float),
+        ("lon", float),
+    ]
+    for key, type_ in key_type_pairs:
+        assert type(json[0][key]) == type_, f"Key {key} is not of type {type_} in first stop data"
+        assert type(json[1][key]) == type_, f"Key {key} is not of type {type_} in second stop data"
