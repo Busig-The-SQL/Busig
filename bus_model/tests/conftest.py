fc92a01bc0fa337944fa07b68f658b35ef9902a8
Liam Cotter <122333311@umail.ucc.ie>
2025-04-10 12:29:49 +0100

    Add session as scope for fixture.

--- bus_model/tests/conftest.py
+++ bus_model/tests/conftest.py
@@ -184,7 +184,7 @@ def shape_filled_instance(vars):
     shape.add_point(51.882502, -8.635329, 0, 200.2)
     yield shape
 
-@pytest.fixture(autouse=True)
+@pytest.fixture(autouse=True, scope="session")
 def all_test_objects(
     bus_instance,
     route_instance,
