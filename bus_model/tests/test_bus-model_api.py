56d4005d45ce61cbb10c9a5fb4ac9296fda1009d
Liam Cotter <122333311@umail.ucc.ie>
2025-04-28 21:51:35 +0100

    Add assertion for  stop predictions when it returns schedule instead.

--- bus_model/tests/test_bus-model_api.py
+++ bus_model/tests/test_bus-model_api.py
@@ -1,16 +1,19 @@
 from flask.testing import FlaskClient
 
+
 def test_home(client: FlaskClient) -> None:
     """Test the / endpoint. It returns a TestResponse instead of a Request Response, so we must use response.json"""
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == {'message': 'Hello, World!'}
 
+
 def test_v1_health(client: FlaskClient) -> None:
     """Test the /v1/health endpoint. It returns a TestResponse instead of a Request Response, so we must use response.json"""
     response = client.get('/v1/health')
     assert response.status_code == 200
     assert response.text in "01", "Invalid health response"
+
 
 def test_v1_get_all_stops(client: FlaskClient) -> None:
     """Tests the /v1/stops endpoint."""
@@ -24,7 +27,7 @@ def test_v1_get_all_stops(client: FlaskClient) -> None:
     for key in keys:
         assert key in json[0], f"Key {key} not in first stop data"
         assert key in json[1], f"Key {key} not in second stop data"
-    
+
     key_type_pairs = [
         ("id", str),
         ("name", str),
@@ -33,8 +36,10 @@ def test_v1_get_all_stops(client: FlaskClient) -> None:
         ("lon", float),
     ]
     for key, type_ in key_type_pairs:
-        assert type(json[0][key]) == type_, f"Key {key} is not of type {type_} in first stop data"
-        assert type(json[1][key]) == type_, f"Key {key} is not of type {type_} in second stop data"
+        assert type(
+            json[0][key]) == type_, f"Key {key} is not of type {type_} in first stop data"
+        assert type(
+            json[1][key]) == type_, f"Key {key} is not of type {type_} in second stop data"
 
 
 def test_v1_get_one_stop(client: FlaskClient, vars) -> None:
@@ -44,17 +49,29 @@ def test_v1_get_one_stop(client: FlaskClient, vars) -> None:
     json = response.json
     assert type(json) == dict, f"Response is not a dict, {type(json)} instead"
 
-    assert json.get("id", "") == vars.stop_id1, f"Stop id {json.get('id', '')} != {vars.stop_id1}"
-    assert json.get("name", "") == "IDA Ovens", f"Stop name {json.get('name', '')} != IDA Ovens"
-    assert json.get("code", "") == vars.stop_code1, f"Stop code {json.get('code', '')} != {vars.stop_code1}"
-    assert type(json.get("lat", "")) == float, f"Stop lat {json.get('lat', '')} is not a float"
-    assert type(json.get("lon", "")) == float, f"Stop lon {json.get('lon', '')} is not a float"
-    assert json.get("direction", "") == 0, f"Stop direction {json.get('direction', '')} != 0"
+    assert json.get(
+        "id", "") == vars.stop_id1, f"Stop id {json.get('id', '')} != {vars.stop_id1}"
+    assert json.get(
+        "name", "") == "IDA Ovens", f"Stop name {json.get('name', '')} != IDA Ovens"
+    assert json.get(
+        "code", "") == vars.stop_code1, f"Stop code {json.get('code', '')} != {vars.stop_code1}"
+    assert type(json.get("lat", "")
+                ) == float, f"Stop lat {json.get('lat', '')} is not a float"
+    assert type(json.get("lon", "")
+                ) == float, f"Stop lon {json.get('lon', '')} is not a float"
+    assert json.get(
+        "direction", "") == 0, f"Stop direction {json.get('direction', '')} != 0"
 
-def test_v1_stop_predictions(client: FlaskClient, vars) -> None:
+
+def test_v1_stop_predictions_missing_return_schedule(client: FlaskClient, vars) -> None:
     """Tests the /v1/stop/arrivals/<stop_id> endpoint. No valid predictions"""
     response = client.get(f'/v1/stop/arrivals/{vars.stop_id1}')
     assert response.status_code == 200
     json = response.json
-    print("JSON", json)
-    assert 1 == 0
+
+    assert json == [{
+        "arrival": '08:00',
+        "route": '220',
+        "headsign": 'Carrigaline',
+        "id": vars.bus_id
+    }], "Incorrect or incomplete data response."
